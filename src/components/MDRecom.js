9f6a33ac88ab389dd3016fb26ab9e4a684ffde7f
yoona1110 <gjdbsdk18@icloud.com>
2024-02-08 02:18:08 +0900

    [Feat] 전체보기 버튼 추가

--- src/components/MDRecom.js
+++ src/components/MDRecom.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import styled from "styled-components";
+import palette from "../styles/palette";
 
 const MDRecom = () => {
     const RecomLists = ['간편식·밀키트·샐러드', '과일·견과·쌀', '국·반찬·메인요리', '헤어·바디·구강', '정육·가공육·계란', '생수·음료·커피', '베이커리'];
@@ -7,24 +8,29 @@ const MDRecom = () => {
     const [isClick, setIsClick] = useState(false);
     const [saveIdx, setSaveIdx] = useState(0);
 
-    const handleOnClick = ( e, idx ) => {
+    const handleOnClick = ( idx ) => {
         setSaveIdx(idx);
     }
 
     return (
         <Container>
-            <MDRecomTitleContainer>
-                <MDRecomTitleText>  MD의 추천  </MDRecomTitleText>
-                <RecomList>
-                    {RecomLists.map((item, idx) => (
-                        <RecomItem 
-                            key={idx}
-                            onClick={e => handleOnClick(e, idx)}
-                            isShow={saveIdx === idx}
-                        > {item} </RecomItem>
-                    ))}
-                </RecomList>
-            </MDRecomTitleContainer>
+            <TitleContainer>
+                <TitleText>  MD의 추천  </TitleText>
+            </TitleContainer>
+            <CategoryTitleList>
+                {RecomLists.map((item, idx) => (
+                    <CategoryTitleItem 
+                        key={idx}
+                        onClick={() => handleOnClick(idx)}
+                        isShow={saveIdx === idx}
+                    > {item} 
+                    </CategoryTitleItem>
+                ))}
+            </CategoryTitleList>
+            <ProductList>
+
+            </ProductList>
+            <MoreBtn> 전체보기 </MoreBtn>
         </Container>
     );
 };
@@ -33,14 +39,14 @@ const Container = styled.div`
     margin: 0px auto;
     padding: 40px 0px;
 `;
-const MDRecomTitleContainer = styled.div`
+const TitleContainer = styled.div`
     margin-bottom: 27px;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
 `;
-const MDRecomTitleText = styled.p`
+const TitleText = styled.p`
     position: relative;
     display: flex;
     min-height: 48px;
@@ -52,12 +58,12 @@ const MDRecomTitleText = styled.p`
     letter-spacing: -0.26px;
     font-weight: 500;
 `;
-const RecomList = styled.ul`
+const CategoryTitleList = styled.ul`
     display: flex;
     flex-wrap: wrap;
     place-content: center;
 `;
-const RecomItem = styled.button`
+const CategoryTitleItem = styled.button`
     box-sizing: border-box;
     background-color: ${(props) => (props.isShow ? 'rgb(95, 0, 128)' : 'rgb(247, 247, 247)')};
     color: ${(props) => (props.isShow ? 'rgb(255, 255, 255)' : 'rgb(51, 51, 51)')};
@@ -69,6 +75,21 @@ const RecomItem = styled.button`
     font-size: 14px;
     height: 40px;
     line-height: 16px;
+    cursor: pointer;
+`;
+const ProductList = styled.div`
+    margin: 20px 0 27px;
+`;
+const MoreBtn = styled.button`
+    margin: 0 auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 516px;
+    height: 56px;
+    background-color: ${palette.white};
+    border-radius: 3px;
+    border: 1px solid rgb(227, 227, 227);
     cursor: pointer;
 `;
 export default MDRecom;
